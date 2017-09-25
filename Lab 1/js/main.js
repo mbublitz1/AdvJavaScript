3a53bdc4d3836db6b1f8c0756d6e4781e765b73c
mbublitz1 <mbublitz1@studentmail.nait.ca>
2017-09-24 20:14:07 -0600

    Add prevent default to end of function

--- Lab 1/js/main.js
+++ Lab 1/js/main.js
@@ -1,14 +1,19 @@
 (function($){
-    $('.Search').on('submit', function (e)
+    $('.search').on('submit', function (e)
     {
         console.log('in submit');
         var searchText = $(e.target).find('[name=searchText]').val(),
-        query = 'flickr.photos.search&api_key=a7aa4bea8f141275bc7c69292b51b672&text=' + searchText + '&format=json&nojsoncallback=1';
+        query = 'flickr.photos.search&api_key=86b74687bf9d2559bfdf7a9d46666c79&text=' + searchText + '&format=json&nojsoncallback=1';
+        console.log('https://api.flickr.com/services/rest/?method=' + query);
+         $.getJSON('https://api.flickr.com/services/rest/?method=' + query, function(data)
+         {
 
-        $.getJSON('https://api.flickr.com/services/rest/?method=' + query, function(data)
-        {
-            console.log(data);
-        });
+             var $id = data.
+             console.log(data);
+         });
+
+        e.preventDefault();
     });
 
 }(jQuery));
+
